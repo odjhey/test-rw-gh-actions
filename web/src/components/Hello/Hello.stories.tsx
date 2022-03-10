7ad0ad4416da6c6c6d400fe1e9a21f4a23825cf1
Odelon Jhey Pacalso <odjhey@gmail.com>
2022-03-10 08:50:52 +0800

    init repo

---
+++ web/src/components/Hello/Hello.stories.tsx
@@ -0,0 +1,7 @@
+import Hello from './Hello'
+
+export const generated = () => {
+  return <Hello />
+}
+
+export default { title: 'Components/Hello' }
---
+++ web/src/components/Hello/Hello.tsx
@@ -0,0 +1,10 @@
+const Hello = () => {
+  return (
+    <div>
+      <h2>{'Hello'}</h2>
+      <p>{'Find me in ./web/src/components/Hello/Hello.tsx'}</p>
+    </div>
+  )
+}
+
+export default Hello
